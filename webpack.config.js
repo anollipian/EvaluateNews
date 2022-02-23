b47702309bf148c3e934518906875296779914f2
Rana Mahdy <rana.m.mahdy@gmail.com>
2022-02-23 12:33:12 -0800

    Updated Webpack Version

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,4 @@
 const path = require("path")
 const webpack = require("webpack")
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-    entry: './src/client/index.js',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
 }
